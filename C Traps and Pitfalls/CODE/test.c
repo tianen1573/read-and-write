b7d003965a1dc692a4ab53ddc00f1b5ec8bbcfca
tianenGit <415258758@qq.com>
2023-02-28 16:06:31 +0800

    CTAP_2-28

---
+++ C Traps and Pitfalls/CODE/test.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int func();
+
+double square(x)
+double x;
+{
+	return x * x;
+}
+
+
+//#define EOF -1
+
+char* Malloc(unsigned n)
+{
+	char* p, * malloc(unsigned);
+
+	p = malloc(n);
+
+	if (p == NULL)
+		printf("aaa");
+
+	return p;
+}
+
+main()
+{
+	/*register int c;
+	while ((c = getchar()) != EOF)
+		putchar('a');*/
+
+	//double d = square(2.0);
+
+	int d = (-1) / 2;
+
+	char c = 'ye';
+
+	printf("%c", c);
+}
+
+//int main()
+//{
+//	/*char a = 'a';
+//	printf(&a);
+//
+//	printf('a');
+//
+//	printf("a");*/
+//
+//	/*char a = 'yaa';
+//
+//	printf("%c", a);*/
+//
+//	/*char *a = 'yaaa';
+//
+//	printf("%s", a);*/
+//
+//	/* /* */ 
+//
+//	char c = getchar();
+//
+//	//printf("%d", func(1,2));
+//
+//	return 0;
+//}
+
+//int func(int a, int b)
+//{
+//	return a + b;
+//}
