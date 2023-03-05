a9b042ebe5be805838e7fe09111866d374e0881e
tianenGit <415258758@qq.com>
2023-03-05 19:46:41 +0800

    errorNote

---
+++ C Traps and Pitfalls/CODE/C++Review.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<iostream>
+
+#include"re.h"
+
+int main()
+{
+
+	func();
+
+	return 0;
+}
---
+++ C Traps and Pitfalls/CODE/re.h
@@ -0,0 +1,10 @@
+#pragma once
+
+using namespace std;
+
+void func(int a);
+
+void func(int a = 10)
+{
+	cout << "funca";
+}
