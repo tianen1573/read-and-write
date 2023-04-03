fa557e1b81f93cd6fcd000a8760bb8330bc01ac5
tianenGit <415258758@qq.com>
2023-04-03 17:04:12 +0800

    note

--- C Traps and Pitfalls/CODE/C++Review.cpp
+++ C Traps and Pitfalls/CODE/C++Review.cpp
@@ -1,14 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 
-#include<iostream>
+//#include<iostream>
+//
+//#include"re.h"
+//
+//int main()
+//{
+//
+//	func();
+//
+//	return 0;
+//}
 
-#include"re.h"
+// write your code here cpp
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+class _Test
+{
+public:
+    /*_Test()
+    {
+        cout << "AA" << endl;
+
+    }*/
+    _Test()
+    {
+        cout << "AA" << endl;
+    }
+    //_Test(_Test&) = delete;
+    ~_Test()
+    {
+        cout << "~~~~" << endl;
+    }
+};
+
+_Test Ab()
+{
+    cout << "FUNC" << endl;
+    return _Test();
+}
 
 int main()
 {
+    //int n = 0;
+    //while (cin >> n)
+    //{
+    //    getchar();
+    //    string ans;
+    //    while (n--)
+    //    {
+    //        string str;
+    //        getline(cin, str);
+    //        if (str.find(' ') != string::npos ||
+    //            str.find(',') != string::npos)
+    //            ans += "\"" + str + "\", ";
+    //        else
+    //            ans += str + ", ";
+    //    }
 
-	func();
+    //    ans[ans.size() - 2] = '\0';
+    //    //ans.resize(ans.size() - 2);
+    //    cout << ans << endl;
+    //}
+    
+    _Test Ab();//º¯ÊýÉùÃ÷
+   // _Test B;
+    cout << "·Ö¸î" << endl;
+    //_Test ();
 
-	return 0;
+    return 0;
+
 }
