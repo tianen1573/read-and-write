9373a321b96c8bb7793d356c5988768bb53294f9
tianenGit <415258758@qq.com>
2023-04-03 18:37:56 +0800

    other

--- C Traps and Pitfalls/CODE/C++Review.cpp
+++ C Traps and Pitfalls/CODE/C++Review.cpp
@@ -1,79 +1,136 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-
-//#include<iostream>
+//#define _CRT_SECURE_NO_WARNINGS 1
 //
-//#include"re.h"
+//
+////#include<iostream>
+////
+////#include"re.h"
+////
+////int main()
+////{
+////
+////	func();
+////
+////	return 0;
+////}
+//
+//// write your code here cpp
+//
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//
+//class _Test
+//{
+//public:
+//    /*_Test()
+//    {
+//        cout << "AA" << endl;
+//
+//    }*/
+//    _Test()
+//    {
+//        cout << "AA" << endl;
+//    }
+//    //_Test(_Test&) = delete;
+//    ~_Test()
+//    {
+//        cout << "~~~~" << endl;
+//    }
+//};
+//
+//_Test Ab()
+//{
+//    cout << "FUNC" << endl;
+//    return _Test();
+//}
 //
 //int main()
 //{
+//    //int n = 0;
+//    //while (cin >> n)
+//    //{
+//    //    getchar();
+//    //    string ans;
+//    //    while (n--)
+//    //    {
+//    //        string str;
+//    //        getline(cin, str);
+//    //        if (str.find(' ') != string::npos ||
+//    //            str.find(',') != string::npos)
+//    //            ans += "\"" + str + "\", ";
+//    //        else
+//    //            ans += str + ", ";
+//    //    }
 //
-//	func();
+//    //    ans[ans.size() - 2] = '\0';
+//    //    //ans.resize(ans.size() - 2);
+//    //    cout << ans << endl;
+//    //}
+//    
+//    _Test Ab();//函数声明
+//   // _Test B;
+//    cout << "分割" << endl;
+//    //_Test ();
 //
-//	return 0;
+//    return 0;
+//
 //}
 
-// write your code here cpp
-
-#include<iostream>
-#include<string>
+#pragma warning(disable:4996)
+#include <WinSock2.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
+#pragma comment(lib,"ws2_32.lib") //固定用法
 
-
-class _Test
-{
-public:
-    /*_Test()
-    {
-        cout << "AA" << endl;
-
-    }*/
-    _Test()
-    {
-        cout << "AA" << endl;
-    }
-    //_Test(_Test&) = delete;
-    ~_Test()
-    {
-        cout << "~~~~" << endl;
-    }
-};
-
-_Test Ab()
-{
-    cout << "FUNC" << endl;
-    return _Test();
-}
+uint16_t serverport = 8080;
+std::string serverip = "123.249.100.5";
 
 int main()
 {
-    //int n = 0;
-    //while (cin >> n)
-    //{
-    //    getchar();
-    //    string ans;
-    //    while (n--)
-    //    {
-    //        string str;
-    //        getline(cin, str);
-    //        if (str.find(' ') != string::npos ||
-    //            str.find(',') != string::npos)
-    //            ans += "\"" + str + "\", ";
-    //        else
-    //            ans += str + ", ";
-    //    }
+	// windows 独有的
+	WSADATA WSAData;
+	WORD sockVersion = MAKEWORD(2, 2);
+	if (WSAStartup(sockVersion, &WSAData) != 0)
+		return 0;
 
-    //    ans[ans.size() - 2] = '\0';
-    //    //ans.resize(ans.size() - 2);
-    //    cout << ans << endl;
-    //}
-    
-    _Test Ab();//函数声明
-   // _Test B;
-    cout << "分割" << endl;
-    //_Test ();
+	SOCKET clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (INVALID_SOCKET == clientSocket)
+	{
+		cout << "socket error!";
+		return 0;
+	}
 
-    return 0;
+	sockaddr_in dstAddr;
+	dstAddr.sin_family = AF_INET;
+	dstAddr.sin_port = htons(serverport);
+	dstAddr.sin_addr.S_un.S_addr = inet_addr(serverip.c_str());
 
+	char buffer[1024];
+
+	while (true)
+	{
+		std::string message;
+		std::cout << "请输入# ";
+		std::getline(std::cin, message);
+		sendto(clientSocket, message.c_str(), (int)message.size(), 0, (sockaddr*)&dstAddr, sizeof(dstAddr));
+
+		struct sockaddr_in temp;
+		int len = sizeof(temp);
+		int s = recvfrom(clientSocket, buffer, sizeof buffer, 0, (sockaddr*)&temp, &len);
+		if (s > 0)
+		{
+			buffer[s] = '\0';
+			std::cout << "server echo# " << buffer << std::endl;
+		}
+	}
+
+	// windows 独有
+	closesocket(clientSocket);
+	WSACleanup();
+
+	return 0;
 }
