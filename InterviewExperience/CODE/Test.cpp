08edd5ced1592e239be02c7d05dae9d33057b067
tianenGit <415258758@qq.com>
2023-07-25 22:45:13 +0800

    C++

---
+++ InterviewExperience/CODE/Test.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	void func()
+	{
+		cout << "I am A::func" << endl;
+		func2();
+	}
+	virtual void func1()
+	{
+		cout << "I am A::func1" << endl;
+		func2();
+	}
+private:
+	virtual void func2()
+	{
+		cout << "I am A::func2" << endl;
+	}
+};
+class B : public A
+{
+public:
+	void func() // Òþ²Ø
+	{
+		cout << "I am B::func" << endl;
+		func2();
+	}
+private:
+	virtual void func1() //ÖØÐ´
+	{
+		cout << "I am B::func1" << endl;
+		func2();
+	}
+public:
+	virtual void func2() // ÖØÐ´
+	{
+		cout << "I am B::func2" << endl;
+	}
+};
+
+int main()
+{
+	B b;
+	A *a = &b;
+
+	b.func();
+	//b.func1();
+	a->func();
+	a->func1();
+
+	return 0;
+}
